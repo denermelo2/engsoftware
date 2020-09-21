50cf0b5cbd93d96789b91cd26d417d79693dbd1c
DENER MELO <denermelo2@gmail.com>
2020-09-21 16:06:41 -0300

    Adicionado opções no painel de Usuário

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
+Alterações 21/09/2020
+
+    Adicionado Menu da página Painel de Usuário (Incompleto ainda) Front-end //Dener
 --------------------------------------------------------------
 Alterações Projeto 15/09/2020
 
 Adicionado imagens de telas na pasta images.
 Adicionado SQL do Banco de dados utilizado na pasta database. //Dener
---------------------------------------------------------------
+
 Alterações Projeto 14/09/2020
 
 Adicionado recuperação de senha e validação via token - Nesta função o banco de dados valida se o token ja foi utilizado para recuperar a senha
--- painelUsuario.php
+++ painelUsuario.php
@@ -4,20 +4,48 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="painelUsuario.css">
     <title> Bem vindo </title>
+     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@700&family=Open+Sans&display=swap"
+        rel="stylesheet">
     
 </head>
 
 <body>
     <div class="leftmenu">
     <div class="user_name"><?php session_start(); ?> <?php echo $_SESSION['email']; ?></div>
+
+    <div class="buttons">
+    <input type="button" value="Cadastrar Tipo Animal">
+    <input type="button" value="Cadastrar Animal">
+    <input type="button" value="Cadastrar Raças">
+    <input type="button" value="Cadastrar Serviços">
+    <input type="button" value="Realizar Agendamentos">
+    </div>
+    
+    
+
     </div>
     <div class="container">
         <div class="top">
             <div class="top_right"> <a href ="logout.php"> Sair </a></div>
             <div class = "top_right">Olá, <?php echo $_SESSION['email']; ?></div>
     </div>
+
+
+<div class="content">
+
+<button class="button button1">Adicionar</button>
+<button class="button button2">Alterar</button>
+<button class="button button3">Excluir</button>
+
+    
+
+    </div>
     
 </body>
+
+
+<footer>
+</footer>
 </html>
 
 
